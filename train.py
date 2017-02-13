f0bc939cdb6d09ab2429e11d2271ba94547a357a
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-02-13 20:17:03 +0900

    move VOCDataset to voc.py

--- train.py
+++ train.py
@@ -1,10 +1,6 @@
 #! /usr/bin/env python3
 
 import argparse
-import cv2
-import numpy as np
-import os
-import xml.etree.ElementTree as ET
 
 import chainer
 from chainer import training
@@ -12,55 +8,7 @@ from chainer.training import extensions
 
 from ssd import SSD300
 from multibox import MultiBoxEncoder
-import voc
-
-
-class VOCDataset(chainer.dataset.DatasetMixin):
-
-    def __init__(self, root, size, encoder):
-        self.root = root
-        self.size = size
-        self.encoder = encoder
-
-        self.images = [
-            l.strip() for l in open(os.path.join(
-                self.root, 'ImageSets', 'Main', 'trainval.txt'))]
-
-    def __len__(self):
-        return len(self.images)
-
-    def get_example(self, i):
-        x = cv2.imread(
-            os.path.join(
-                self.root, 'JPEGImages', self.images[i] + '.jpg'),
-            cv2.IMREAD_COLOR)
-        h, w, _ = x.shape
-        x = cv2.resize(x, (self.size, self.size)).astype(np.float32)
-        x -= (103.939, 116.779, 123.68)
-        x = x.transpose(2, 0, 1)
-
-        boxes = list()
-        classes = list()
-        tree = ET.parse(os.path.join(
-            self.root, 'Annotations', self.images[i] + '.xml'))
-        for child in tree.getroot():
-            if not child.tag == 'object':
-                continue
-            bndbox = child.find('bndbox')
-            xmin = float(bndbox.find('xmin').text)
-            ymin = float(bndbox.find('ymin').text)
-            xmax = float(bndbox.find('xmax').text)
-            ymax = float(bndbox.find('ymax').text)
-            boxes.append((
-                (xmin + xmax) / (2 * w),
-                (ymin + ymax) / (2 * h),
-                (xmax - xmin) / w,
-                (ymax - ymin) / h))
-            classes.append(voc.names.index(child.find('name').text))
-        loc, conf = self.encoder.encode(
-            np.array(boxes), np.array(classes))
-
-        return x, loc, conf
+from voc import VOCDataset
 
 
 if __name__ == '__main__':
