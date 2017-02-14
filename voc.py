f92ecc7f224382a40dabef1b329fd20c879f3024
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-02-14 11:21:16 +0900

    retry to crop if no box is included

--- voc.py
+++ voc.py
@@ -49,30 +49,37 @@ class VOCDataset(chainer.dataset.DatasetMixin):
         return len(self.images)
 
     def augment(self, image, boxes, classes):
+        if len(boxes) == 0:
+            raise ValueError('boxes is empty')
+
         h, w, _ = image.shape
 
         mode = random.randrange(2)
-        if mode == 0:
-            patch = (0, 0, w, h)
-        elif mode == 1:
-            size = random.uniform(0.1, 1) * w * h
-            aspect = random.uniform(
-                max(0.5, size / (w * w)),
-                min(2, (h * h) / size))
-            patch_w = np.sqrt(size / aspect)
-            patch_h = np.sqrt(size * aspect)
-            patch_l = random.uniform(0, w - patch_w)
-            patch_t = random.uniform(0, h - patch_h)
-            patch = (patch_l, patch_t, patch_l + patch_w, patch_t + patch_h)
+        while True:
+            if mode == 0:
+                patch = (0, 0, w, h)
+            elif mode == 1:
+                size = random.uniform(0.1, 1) * w * h
+                aspect = random.uniform(
+                    max(0.5, size / (w * w)),
+                    min(2, (h * h) / size))
+                patch_w = np.sqrt(size / aspect)
+                patch_h = np.sqrt(size * aspect)
+                patch_l = random.uniform(0, w - patch_w)
+                patch_t = random.uniform(0, h - patch_h)
+                patch = (
+                    patch_l, patch_t, patch_l + patch_w, patch_t + patch_h)
+
+            centers = (boxes[:, :2] + boxes[:, 2:]) / 2
+            mask = np.logical_and(
+                (patch[:2] <= centers).all(axis=1),
+                (patch[2:] > centers).all(axis=1))
+            if mask.any():
+                break
 
         image = image[
             int(patch[1]):int(patch[3]),
             int(patch[0]):int(patch[2])]
-
-        centers = (boxes[:, :2] + boxes[:, 2:]) / 2
-        mask = np.logical_and(
-            (patch[:2] <= centers).all(axis=1),
-            (patch[2:] > centers).all(axis=1))
         boxes = boxes[mask]
         classes = classes[mask]
 
