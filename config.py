1f4aea7c77130c24513d98b8e87180e88f4ec3b4
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-04-18 01:35:40 +0900

    remove unused param

--- config.py
+++ config.py
@@ -2,5 +2,4 @@ mean = (104, 117, 123)
 scale = 300
 steps = [s / scale for s in (8, 16, 32, 64, 100, 300)]
 sizes = [s / scale for s in (30, 60, 111, 162, 213, 264, 315)]
-aspect_ratios = ((2,), (2, 3), (2, 3), (2, 3), (2,), (2,))
 variance = (0.1, 0.2)
