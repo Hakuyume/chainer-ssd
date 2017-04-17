8c82c0881c3e3a911f093f3bf9588367b3a1e4eb
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-04-18 07:59:33 +0900

    update test

--- test.py
+++ test.py
@@ -6,79 +6,83 @@ import numpy as np
 import os
 
 import chainer
-from chainer import cuda
+from chainer import iterators
 from chainer import serializers
 
-import config
-from multibox import MultiBoxEncoder
-from ssd import SSD300
-from voc import VOC
+from lib import MultiBoxEncoder
+from lib import SSD300
+from lib import VOCDataset
+
+
+class TestDataset(object):
+
+    def __init__(self, dataset, model):
+        self.dataset = dataset
+        self.insize = model.insize
+        self.mean = model.mean
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, i):
+        name = self.dataset.name(i)
+
+        image = self.dataset.image(i)
+        height, width, _ = image.shape
+
+        image = cv2.resize(image, (self.insize, self.insize))
+        image = image.astype(np.float32)
+        image -= self.mean
+        image = image.transpose(2, 0, 1)
+
+        return name, image, (width, height)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--root', default='VOCdevkit')
     parser.add_argument('--output', default='.')
-    parser.add_argument('--test', action='append')
+    parser.add_argument('--test')
     parser.add_argument('--batchsize', type=int, default=32)
     parser.add_argument('--gpu', type=int, default=-1)
     parser.add_argument('model')
     args = parser.parse_args()
 
-    model = SSD300(n_classes=20, aspect_ratios=config.aspect_ratios)
+    model = SSD300(20)
     serializers.load_npz(args.model, model)
     if args.gpu >= 0:
         chainer.cuda.get_device(args.gpu).use()
         model.to_gpu()
-        xp = cuda.cupy
-    else:
-        xp = np
 
-    multibox_encoder = MultiBoxEncoder(
-        grids=model.grids,
-        steps=config.steps,
-        sizes=config.sizes,
-        aspect_ratios=model.aspect_ratios,
-        variance=config.variance)
+    multibox_encoder = MultiBoxEncoder(model)
 
-    def dump_result(name, size, loc, conf):
-        print(name)
+    year, subset = args.test.split('-')
+    dataset = TestDataset(VOCDataset(args.root, year, subset), model)
 
-        if xp is not np:
-            loc = xp.asnumpy(loc)
-            conf = xp.asnumpy(conf)
+    iterator = iterators.SerialIterator(
+        dataset, args.batchsize, repeat=False, shuffle=False)
 
-        boxes, conf = multibox_encoder.decode(loc, conf)
-        nms = multibox_encoder.non_maximum_suppression(boxes, conf, 0.45, 0.01)
-        for box, cls, score in nms:
-            box *= size
-            filename = os.path.join(
-                args.output, 'comp4_det_test_{:s}.txt'.format(VOC.names[cls]))
-            with open(filename, mode='a') as f:
-                print(
-                    name, score, box.left, box.top, box.right, box.bottom,
-                    file=f)
+    while True:
+        try:
+            batch = next(iterator)
+        except StopIteration:
+            break
 
-    dataset = VOC(args.root, [t.split('-') for t in args.test])
+        x = np.stack([image for _, image, _ in batch])
+        loc, conf = model(x)
+        loc = chainer.cuda.to_cpu(loc.data)
+        conf = chainer.cuda.to_cpu(conf.data)
 
-    info = list()
-    batch = list()
-    for i in range(len(dataset)):
-        src = dataset.image(i)
-        info.append((dataset.name(i), src.shape[1::-1]))
-        x = cv2.resize(src, (model.insize, model.insize)).astype(np.float32)
-        x -= config.mean
-        x = x.transpose(2, 0, 1)
-        batch.append(x)
+        for (name, _, size), loc, conf in zip(batch, loc, conf):
+            print(name)
 
-        if len(batch) == args.batchsize:
-            loc, conf = model(chainer.Variable(xp.array(batch), volatile=True))
-            for i, (name, size) in enumerate(info):
-                dump_result(name, size, loc.data[i], conf.data[i])
-            info = list()
-            batch = list()
+            results = multibox_encoder.decode(loc, conf, 0.45, 0.01)
+            for box, label, score in results:
+                box[:2] *= size
+                box[2:] *= size
 
-    if len(batch) > 0:
-        loc, conf = model(chainer.Variable(xp.array(batch), volatile=True))
-        for i, (name, size) in enumerate(info):
-            dump_result(name, size, loc.data[i], conf.data[i])
+                path = os.path.join(
+                    args.output,
+                    'comp4_det_test_{:s}.txt'.format(VOCDataset.labels[label]))
+                with open(path, mode='a') as f:
+                    print(name, score, *box, file=f)
