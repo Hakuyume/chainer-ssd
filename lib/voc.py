2e5c6059c0af753abba43eec9dcd56124f2f43d7
Toru Ogawa <Hakuyume@users.noreply.github.com>
2017-04-18 07:59:22 +0900

    update VOCDataset

--- lib/voc.py
+++ lib/voc.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import os
 import xml.etree.ElementTree as ET
 
@@ -28,42 +29,39 @@ class VOCDataset(object):
         'tvmonitor',
     )
 
-    def __init__(self, root, sets):
-        self.root = root
+    def __init__(self, root, year, subset):
+        self.root = os.path.join(self.root, 'VOC' + year)
 
-        self.images = list()
-        for year, name in sets:
-            root = os.path.join(self.root, 'VOC' + year)
-            for line in open(
-                    os.path.join(root, 'ImageSets', 'Main', name + '.txt')):
-                self.images.append((root, line.strip()))
+        path = os.path.join(self.root, 'ImageSets', 'Main', subset + '.txt')
+        self.images = [line.strip() for line in open(path)]
 
     def __len__(self):
         return len(self.images)
 
     def name(self, i):
-        return self.images[i][1]
+        return self.images[i]
 
     def image(self, i):
         return cv2.imread(
-            os.path.join(
-                self.images[i][0], 'JPEGImages', self.images[i][1] + '.jpg'),
+            os.path.join(self.root, 'JPEGImages', self.images[i] + '.jpg'),
             cv2.IMREAD_COLOR)
 
     def annotations(self, i):
-        annotations = list()
-
         tree = ET.parse(os.path.join(
-            self.images[i][0], 'Annotations', self.images[i][1] + '.xml'))
+            self.root, 'Annotations', self.images[i] + '.xml'))
+
+        boxes = list()
+        labels = list()
         for child in tree.getroot():
             if not child.tag == 'object':
                 continue
             bndbox = child.find('bndbox')
-            bbox = tuple(
+            box = tuple(
                 float(bndbox.find(t).text) - 1
                 for t in ('xmin', 'ymin', 'xmax', 'ymax'))
             label = self.labels.index(child.find('name').text)
 
-            annotations.append((bbox, label))
+            boxes.append(box)
+            labels.append(label)
 
-        return annotations
+        return np.array(boxes), np.array(labels)
